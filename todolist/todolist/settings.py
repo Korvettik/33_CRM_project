12572b059f579541a94bfae9603c217b36fd3c76
Korvettik <korvet1988@yandex.ru>
2022-09-27 21:01:32 +0300

    .env added

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -12,6 +12,23 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
+
+
+# ###################################################
+
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# ##################################################
+
+
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,14 +36,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ur!@xb6^4!vm*$!p*66wb0c3ra$^nv9d0&*w=_fgf9cofm+5j('
+# SECRET_KEY = 'django-insecure-ur!@xb6^4!vm*$!p*66wb0c3ra$^nv9d0&*w=_fgf9cofm+5j('
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
 
+
+
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
